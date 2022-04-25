cd5a96a7877552ff419911cb02b8208ca1a18d2c
Mat Werber <mat@releasehub.com>
2022-04-25 06:00:01 -0700

    fix typos

--- examples/aws-s3-backend/bin/init.sh
+++ examples/aws-s3-backend/bin/init.sh
@@ -4,9 +4,9 @@ set -e
 echo "[BEGIN TERRAFORM INIT]"
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-echo "AWS Account IDL $ACCOUNT_ID"
+echo "AWS Account ID = $ACCOUNT_ID"
 
-export DEPLOYMENT_ROLE_ARN="arn:aws:iam::$ACCOUNT_ID:role/$TERRAFORM_DEPLOYMENT_ROLE_NAME"
+export DEPLOYMENT_ROLE_ARN="arn:aws:iam::${ACCOUNT_ID}:role/${TERRAFORM_DEPLOYMENT_ROLE_NAME}"
 echo "Assuming role $DEPLOYMENT_ROLE_ARN..."
 # Assume the role needed to read/write backend state and deploy stuff with terraform:
 aws_credentials=$(aws sts assume-role --role-arn $DEPLOYMENT_ROLE_ARN --role-session-name "release-$RELEASE_APP_NAME-$RELEASE_ENV_ID-terraform")
@@ -27,7 +27,7 @@ export TERRAFORM_STATE_OBJECT_KEY="release/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAM
 TERRAFORM_STATE_BUCKET_NAME="$TERRAFORM_STATE_BUCKET_NAME_PREFIX-$ACCOUNT_ID"
 
 echo "Terraform state file will be kept at the following S3 bucket:"
-echo "s3://$TERRAFORM_STATE_BUCKET_NAME/$TERRAFORM_STATE_OBJECT_KEY ($TERRAFORM_STATE_BUCKET_REGION)
+echo "s3://$TERRAFORM_STATE_BUCKET_NAME/$TERRAFORM_STATE_OBJECT_KEY ($TERRAFORM_STATE_BUCKET_REGION)"
 
 terraform init -migrate-state -force-copy \
     -backend-config="key=$TERRAFORM_STATE_OBJECT_KEY" \
