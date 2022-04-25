1658ca7abd59e3137e22726f21caec5f29adbb1c
Mat Werber <mat@releasehub.com>
2022-04-25 07:25:22 -0700

    update

--- examples/aws-s3-backend/bin/init.sh
+++ examples/aws-s3-backend/bin/init.sh
@@ -29,6 +29,13 @@ TERRAFORM_STATE_BUCKET_NAME="$TERRAFORM_STATE_BUCKET_NAME_PREFIX-$ACCOUNT_ID"
 echo "Terraform state file will be kept at the following S3 bucket:"
 echo "s3://$TERRAFORM_STATE_BUCKET_NAME/$TERRAFORM_STATE_OBJECT_KEY ($TERRAFORM_STATE_BUCKET_REGION)"
 
+# Notice that a backend table (i.e. DynamoDB) is *not* present, which means statefiles cannot be locked.
+# In production or other permanent environments, being able to lock state would typically be a good thing.
+# However, with an ephemeral, stateless environment, we don't want to lock files because, if a job fails
+# due to an error in the template, re-running the deployment after you find and fix the template error
+# will still fail until you find and remove the proper item that represents the lock. When using ephemeral
+# environments that are inherently more commonly used for experimentation, such template errors are
+# also more common. By foregoing a lock table, we avoid adding unnecessary operational overhead:
 terraform init -migrate-state -force-copy \
     -backend-config="key=$TERRAFORM_STATE_OBJECT_KEY" \
     -backend-config="bucket=$TERRAFORM_STATE_BUCKET_NAME" \
