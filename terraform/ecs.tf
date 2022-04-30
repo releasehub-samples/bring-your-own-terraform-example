594a77c022f60514ac7fca1568c0c7c5d6e3a0bc
Mat Werber <mat@releasehub.com>
2022-04-29 17:42:14 -0700

    add missing alb

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,7 @@
+### ECS example primarily from the link below:
+# https://registry.terraform.io/modules/umotif-public/ecs-fargate/aws/latest
+
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -9,8 +13,52 @@ data "aws_subnets" "all" {
   }
 }
 
-### ECS example adapted from the link below (refer to it or detail):
-# https://registry.terraform.io/modules/umotif-public/ecs-fargate/aws/latest
+#####
+# ALB
+#####
+module "alb" {
+  source  = "umotif-public/alb/aws"
+  version = "~> 2.0"
+
+  name_prefix        = "release-ecs-fargate-demo"
+  load_balancer_type = "application"
+  internal           = false
+  vpc_id             = data.aws_vpc.default.id
+  subnets            = data.aws_subnets.all.ids
+}
+
+resource "aws_lb_listener" "alb_80" {
+  load_balancer_arn = module.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = module.ecs-fargate.target_group_arn[0]
+  }
+}
+
+#####
+# Security Group Config
+#####
+resource "aws_security_group_rule" "alb_ingress_80" {
+  security_group_id = module.alb.security_group_id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "task_ingress_80" {
+  security_group_id        = module.fargate.service_sg_id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = module.alb.security_group_id
+}
 
 resource "aws_ecs_cluster" "cluster" {
   name = "release-terraform-demo"
